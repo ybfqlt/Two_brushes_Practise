ea684346001b08320ee7eff7c07d99c2dd33f4c7
ltt <823030735@qq.com>
2019-10-26 23:00:11 +0800

    自定义查询

--- xns-elasticsearch/src/main/java/com/xns/elasticsearch/ItemRepository.java
+++ xns-elasticsearch/src/main/java/com/xns/elasticsearch/ItemRepository.java
@@ -3,6 +3,8 @@ package com.xns.elasticsearch;
 import com.xns.elasticsearch.pojo.Item;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 /**
  * @Classname ItemRepository
  * @Description TODO
@@ -10,4 +12,7 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
  * @Created by xns
  */
 public interface ItemRepository extends ElasticsearchRepository<Item,Long> {
+    List<Item> findByTitle(String title);
+
+    List<Item> findByPriceBetween(Double d1,Double d2);
 }
--- xns-elasticsearch/src/test/java/com/xns/elasticsearch/ElasticsearchTest.java
+++ xns-elasticsearch/src/test/java/com/xns/elasticsearch/ElasticsearchTest.java
@@ -1,12 +1,17 @@
 package com.xns.elasticsearch;
 
 import com.xns.elasticsearch.pojo.Item;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -50,5 +55,59 @@ public class ElasticsearchTest {
     public void testFind(){
         Iterable<Item> items = this.itemRepository.findAll(Sort.by("price").descending());
         items.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindByTitle(){
+        List<Item> items = this.itemRepository.findByTitle("手机");
+        items.forEach(System.out::println);
+    }
+
+    @Test
+    public void testFindByPriceBetween(){
+        List<Item> items = this.itemRepository.findByPriceBetween(3699d,4499d);
+        items.forEach(System.out::println);
+    }
+
+    @Test
+    public void indexList(){
+        List<Item> list = new ArrayList<>();;
+        list.add(new Item(1L,"小米手机7","手机","小米",3299.00,"http://image.nice.com/3232.jpg"));
+        list.add(new Item(2L,"坚果手机R1","手机","锤子",3699.00,"http://image.nice.com/3232.jpg"));
+        list.add(new Item(3L,"华为META10","手机","华为",4499.00,"http://image.nice.com/3232.jpg"));
+        list.add(new Item(4L,"小米Mix2s","手机","小米",4299.00,"http://image.nice.com/3232.jpg"));
+        list.add(new Item(5L,"荣耀V10","手机","华为",2799.00,"http://image.nice.com/3232.jpg"));
+        itemRepository.saveAll(list);
+    }
+
+
+    @Test
+    public void testSearch(){
+        //通过查询构建器工具构建查询条件
+        MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("title", "手机");
+        Iterable<Item> search = this.itemRepository.search(matchQueryBuilder);
+        search.forEach(System.out::println);
+    }
+
+    /**
+     * 自定义查询
+     */
+    @Test
+    public void testNative(){
+        /**
+         * 构建子弟那个一查询构建器
+         */
+       NativeSearchQueryBuilder nativeSearchQuery = new NativeSearchQueryBuilder();
+        /**
+         * 添加基本的查询条件
+         */
+       nativeSearchQuery.withQuery(QueryBuilders.matchQuery("title", "手机"));
+        /**
+         * 执行查询获取分页结果集，不分页，默认为一页
+         */
+       Page<Item> search = this.itemRepository.search(nativeSearchQuery.build());
+       System.out.println(search.getTotalPages());
+       System.out.println(search.getTotalElements());
+       search.forEach(System.out::println);
     }
 }
