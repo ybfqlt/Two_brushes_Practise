aa6aca207d8a4d5db21e333bdebf33ff7773a1e6
ltt <823030735@qq.com>
2019-11-02 17:37:26 +0800

    Quartz了解练习

---
+++ xns_quartz/src/main/java/com/example/quartz/XnsQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.example.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XnsQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XnsQuartzApplication.class, args);
+	}
+
+}
