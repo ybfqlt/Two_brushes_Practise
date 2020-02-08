63554f5d37e5f40af95484bc2d4e59450869a3f9
ltt <823030735@qq.com>
2020-02-08 23:44:11 +0800

    test

---
+++ practise_tests/src/com/ns/demo1/test.java
@@ -0,0 +1,20 @@
+package com.ns.demo1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: xns
+ * @Date: 20-2-8 下午11:14
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class test {
+}
